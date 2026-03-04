c349889d45488e3ca9f45fc65d994f4308238589
AGBEVE Ayao Nicodème <nicdeme.agbeve@gmail.com>
2026-03-04 13:01:24 +0000

    j'avance2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,13 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create()
-
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->trustProxies(at: '*');
-    });
+        // C'est ici que l'on règle l'erreur de votre formulaire non sécurisé
+        $middleware->trustProxies(at: '*'); 
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
