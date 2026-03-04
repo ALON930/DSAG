9e80f9d385d89009de768be57009f30682dfa6d5
AGBEVE Ayao Nicodème <nicdeme.agbeve@gmail.com>
2026-03-04 11:26:34 +0000

    projet pret a finalisez

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })->create()
+
+    ->withMiddleware(function (Middleware $middleware) {
+    $middleware->trustProxies(at: '*');
+    });
